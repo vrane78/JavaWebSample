30a1248ebae454469136bf1dcc8873e30deca8d5
vrane78 <vrane@penielsolutions.com>
2019-02-21 16:54:56 -0500

    Create TestMetrix.java

---
+++ src/test/java/com/example/javamavenjunithelloworld/TestMetrix.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestMetrix {
+
+   private Metrix h;
+	
+   @Before
+   public void setUp() throws Exception 
+   {
+      h = new Metrix();
+   }
+
+   @Test
+   public void testHelloEmpty() 
+   {
+      assertEquals(h.getName(),"");
+      assertEquals(h.getMessage(),"Hello!");
+   }
+	
+   @Test
+   public void testHelloWorld() 
+   {
+      h.setName("World");
+      assertEquals(h.getName(),"World");
+      assertEquals(h.getMessage(),"Hello World!");
+   }
+}
