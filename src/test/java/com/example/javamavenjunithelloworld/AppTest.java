f20a398ab3d462b59dfba175fd63e82ddc048e9c
vrane78 <vrane@penielsolutions.com>
2019-02-21 17:10:20 -0500

    Update AppTest.java

--- src/test/java/com/example/javamavenjunithelloworld/AppTest.java
+++ src/test/java/com/example/javamavenjunithelloworld/AppTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
  
-import com.howtodoinjava.junit5.examples.Calculator;
  
 public class AppTest {
      
